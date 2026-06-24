4edb1bdc72d942e77e86a2596dfb88ba79c18c52
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-06-24 18:53:33 +0000

    Refactor Content component to rend Part components

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -7,15 +7,9 @@
   const Content = (props) => {
     return (
     <>
-       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercise={props.exercise1}/>
+      <Part part={props.part2} exercise={props.exercise2}/>
+      <Part part={props.part3} exercise={props.exercise3}/>
     </>
     )
   }
@@ -26,24 +20,29 @@
     )
   }
 
+  const Part = (props) => {
+    return (
+      <p> {props.part} {props.exercise} </p>
+    )
+  }
 
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
-  const exercises1 = 10
+  const exercise1 = 10
   const part2 = 'Using props to pass data'
-  const exercises2 = 7
+  const exercise2 = 7
   const part3 = 'State of a component'
-  const exercises3 = 14
+  const exercise3 = 14
 
-  const total = exercises1 + exercises2 + exercises3
+  const total = exercise1 + exercise2 + exercise3
 
  
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content part1={part1} part2={part2} part3={part3} exercise1={exercise1} exercise2={exercise2} exercise3={exercise3}/>
       <Total total={total}/>
     </div>
   )
