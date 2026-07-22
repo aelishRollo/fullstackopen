22126aa2263db8fae33f9b30e95f71348be42b45
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-07-22 19:10:46 +0000

    Conditional Rendering

--- part1/courseinformation/src/App.jsx
+++ part1/courseinformation/src/App.jsx
@@ -1,10 +1,12 @@
- const Header = (props) => {
+import {useState} from 'react'
+
+const Header = (props) => {
     return (
       <h1>{props.course.name}</h1>
     )
   }
 
-  const Content = (props) => {
+const Content = (props) => {
     return (
     <>
       <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises}/>
@@ -14,46 +16,61 @@
     )
   }
 
-  const Total = (props) => {
+const Total = (props) => {
     const total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
     return (
       <p>Number of exercises {total}</p>
     )
   }
 
-  const Part = (props) => {
+const Part = (props) => {
     return (
       <p> {props.part} {props.exercises} </p>
     )
   }
 
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return(
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return(
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10 
-      }, 
-      {
-        name: 'Using props to pass data',
-        exercises: 7 
-      }, 
-      {
-        name: 'State of a component',
-        exercises: 14 
-      }
-    ]
-  } 
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
+  }
 
- 
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <History allClicks={allClicks} />
     </div>
   )
 }
