9c1c48ea6b87732e834daba49ff3e43a4e13d82b
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-07-26 12:14:11 -0400

    2.1 done

--- part2/part2/src/App.jsx
+++ part2/part2/src/App.jsx
@@ -1,17 +1,46 @@
-import Note from './components/Note'
 
-function App({notes}) {
-  
+const Course = ({course}) => {
+  const {id, name, parts} = course
+  console.log(parts)
   return (
     <div>
-    <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note}/>
-        )}
-      </ul>
+      <h1>{name}</h1>
+      <div>
+        {parts.map((part) => {
+          return (
+            <h3 key={part.id}>{part.name} {part.exercises}</h3>
+          )
+        })
+        }
+      </div>
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
