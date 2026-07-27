0049ce5868aa9db1723d4d502c9a09e787bcb63e
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-07-27 06:35:39 -0400

    Add controlled component

--- part2/part2/src/App.jsx
+++ part2/part2/src/App.jsx
@@ -1,6 +1,25 @@
 import Note from './components/Note'
+import { useState } from 'react'
 
-const App = ({ notes }) => {
+
+
+const App = (props) => {
+  
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked')
+    console.log(event.target)
+  }
+
+  const [notes, setNotes] = useState(props.notes)
+
+  const [newNote, setNewNote] = useState('This is a new note...')
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -9,6 +28,10 @@ const App = ({ notes }) => {
           <Note key={note.id} note={note} />
         ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='Submit'>Save </button>
+      </form>
     </div>
   )
 }
