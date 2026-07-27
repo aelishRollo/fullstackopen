ac59c1700ba1b4aad725a6648bbc5932a7bbb689
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-07-27 15:12:12 +0000

    Prepare for 2.6

--- part2/part2/src/App.jsx
+++ part2/part2/src/App.jsx
@@ -5,16 +5,30 @@ import { useState } from 'react'
 
 const App = (props) => {
   
+  //for when you hit submit. Adds new note to notes and resets the text in the input
   const addNote = (event) => {
     event.preventDefault()
     console.log('button clicked')
     console.log(event.target)
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1)
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+
   }
 
   const [notes, setNotes] = useState(props.notes)
-
   const [newNote, setNewNote] = useState('This is a new note...')
+  const [showAll, setShowAll] = useState(true)
 
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  //for handling typing into the input box
   const handleNoteChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value)
@@ -23,8 +37,13 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={(() => setShowAll(!showAll))}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
