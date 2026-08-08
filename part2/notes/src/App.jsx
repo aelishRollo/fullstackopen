ac5cf54e66e59dfbef6d765ab4b9978daee1e21c
Alec Rollison <88508028+aelishRollo@users.noreply.github.com>
2026-08-08 01:41:27 -0400

    Ready for changing importance of notes

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -25,8 +25,15 @@ const App = () => {
       id: String(notes.length + 1),
     }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(res => {
+      setNotes(notes.concat(res.data))
+      setNewNote('')
+    })
+
+    
+
   }
 
   const handleNoteChange = event => {
